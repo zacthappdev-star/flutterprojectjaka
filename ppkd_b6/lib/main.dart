de4f5aac055e940a6a29168df38d68239d96fec1
zacthappdev <zacthappdev@gmail.com>
2026-05-08 09:13:57 +0700

    tugas flutter

--- ppkd_b6/lib/main.dart
+++ ppkd_b6/lib/main.dart
@@ -28,9 +28,9 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: .fromSeed(seedColor: const Color.fromARGB(255, 183, 58, 58)),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Demo Home Pag'),
     );
   }
 }
